52575b333f19b261a89b01a8230755834cf8e49c
Vladimir Korobtsov <korobtsov.v@slm.ua>
2023-01-22 13:51:29 +0200

    script1.sh - rework

--- script1.sh
+++ script1.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 
 # (c)Volodymyr Korobtsov
-# Linux & Bash Task
+# Linux & Bash Task #1
 
 allargs="Aviable arguments:
 --all       - displays the IP addresses and symbolic names of all hosts in the current subnet
 --target    - displays a list of open system TCP ports"
 
 function getAllHosts {
-    /usr/sbin/arp -a
+    echo "Hosts in local network:"
+    /usr/sbin/arp -a | awk '{print $2, $1}'
 }
 function getPorts {
-    /usr/bin/netstat -tln
+    echo "Open TCP ports:"
+    /usr/bin/netstat -t4ln | awk '{print $4}' | awk -F":" '{print $NF}' | tail +3
 }
 
 if [[ $# = 0 ]]; then
     echo "Please input some argument."
     echo "$allargs"
 else
-    if [[ $1 == '--all' ]]; then
-        getAllHosts
-    elif [[ $1 == '--target' ]]; then
-        getPorts
-    else
-        echo "Wrong argument!"
-        echo "$allargs"
-    fi
+    for arg in $@; do
+        if [ "$arg" == "--all" ] || [ "$arg" == "--target" ]; then
+            continue
+        else
+            echo "Wrong argument! $arg"
+            echo "$allargs"
+            break
+        fi
+
+    done
+    for arg in $@; do
+        if [[ $arg == '--all' ]]; then
+            getAllHosts
+        elif [[ $arg == '--target' ]]; then
+            getPorts
+        fi
+    done
 fi
