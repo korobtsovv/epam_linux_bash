547fcacdf5d5861b9b6aefabf1b7b900a0dfb076
Vladimir Korobtsov <korobtsov.v@slm.ua>
2023-01-22 13:58:51 +0200

    script1 - fix error exit

--- script1.sh
+++ script1.sh
@@ -16,20 +16,22 @@ function getPorts {
     /usr/bin/netstat -t4ln | awk '{print $4}' | awk -F":" '{print $NF}' | tail +3
 }
 
+# Check arguments number
 if [[ $# = 0 ]]; then
     echo "Please input some argument."
     echo "$allargs"
 else
+    # Check for wrong arguments
     for arg in $@; do
         if [ "$arg" == "--all" ] || [ "$arg" == "--target" ]; then
             continue
         else
-            echo "Wrong argument! $arg"
+            echo "ERROR! Wrong argument - '$arg'"
             echo "$allargs"
-            break
+            exit 1
         fi
-
     done
+    # Run script
     for arg in $@; do
         if [[ $arg == '--all' ]]; then
             getAllHosts
